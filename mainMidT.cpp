a05a661c078243d2cbe4c750d7c982463abd4c08
klausskent <klausskent17@gmail.com>
2017-11-10 13:57:16 -0500

    Midterm main

---
+++ mainMidT.cpp
@@ -0,0 +1,40 @@
+//Name    : Egide Capo-chichi
+//Creation: 11.08.17
+//Version : v1
+//Description:Simple calculator that make 4 operations and  Quits program when user request it. Main function just take a single function
+
+
+#include <iostream>
+#include <string>
+
+
+#include "ccalculator.h"
+
+using namespace std;
+
+
+
+// Name: Main
+//Description: Make a single function call
+//Return: 0 when it is successful
+//Parameters: None
+
+
+int main()
+{
+
+  calculator (); // calling calculator function from ccalculator file
+
+  return 0;
+}
+
+
+ //Name: Main
+ //Return: int
+ //Parameters:none
+ //Flowchart:
+ //Label                            Task                                      goto
+ //01                               (start)                                   02
+ //02                               Calculator();                             03
+ //03                               (exit: 0)
+ 
